2e2325afc05093d7ed8e73a20b2c1932058db1ec
samrat <samrat.keshri.24@gmail.com>
2021-01-05 00:50:41 +0530

    adding add list

--- venv/Arrays/LinkedList.py
+++ venv/Arrays/LinkedList.py
@@ -7,6 +7,7 @@ class LinkedList:
     def __init__(self):
         self.head = None
 
+    #printing the value of the list
     def print_LL(self):
         if self.head is None:
             print("Linked list is empty")
@@ -16,5 +17,8 @@ class LinkedList:
                 print(n.data)
                 n=n.ref
 
-ll = LinkedList()
-ll.print_LL()
+    # adding in the linked list
+    def add_begin(self, data):
+        new_node = Node(data)
+        new_node.ref = self.head
+        self.head = new_node
