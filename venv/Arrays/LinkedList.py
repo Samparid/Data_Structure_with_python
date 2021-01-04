702d4b97bb102858dea85aa36ad564a43b640fba
samrat <samrat.keshri.24@gmail.com>
2021-01-05 00:39:01 +0530

    LinkedList

---
+++ venv/Arrays/LinkedList.py
@@ -0,0 +1,20 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.ref = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def print_LL(self):
+        if self.head is None:
+            print("Linked list is empty")
+        else:
+            n = self.head
+            while n is not None:
+                print(n.data)
+                n=n.ref
+
+ll = LinkedList()
+ll.print_LL()
