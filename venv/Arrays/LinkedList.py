27da816b715ef154974fadd7df2b2406c25fdba3
samrat <samrat.keshri.24@gmail.com>
2021-01-06 20:59:41 +0530

    added end support

--- venv/Arrays/LinkedList.py
+++ venv/Arrays/LinkedList.py
@@ -22,3 +22,14 @@ class LinkedList:
         new_node = Node(data)
         new_node.ref = self.head
         self.head = new_node
+
+    # adding from the end
+    def add_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            n = self.head
+            while n.ref is not None:
+                n = n.ref
+            n.ref = new_node
